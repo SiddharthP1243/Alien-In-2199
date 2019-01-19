00763d9212a5e8b1d10d567901de6062344275d6
SiddharthP1243 <46835543+SiddharthP1243@users.noreply.github.com>
2019-01-19 18:58:03 +0530

    Delete shooter game for codefest(sprite coding).py

--- shooter game for codefest(sprite coding).py
+++
@@ -1,9 +0,0 @@
-import pygame
-import sys
-
-pygame.init()
-walkright = (pygame.image.load("th1")pygame.image.load("th2"))
-walkleft = (pygame.image.load("th(8)")pygame.image.load("th(9)"))
-bg = (pygame.image.load("th(4)")pygame.image.load("th(3)")pygame.image.load("th(5)")pygame.image.load("th(6)"))
-alien = (pygame.image.load("th(1)")pygame.image.load("th(2)"))
-laser = (pygame.image.load("th(7)")pygame.image.load("laser(1)"))
